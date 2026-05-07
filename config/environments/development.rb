c78b63afc988a2317e5991781c33d893a3039c03
SeradedStripes <seradedstripes@gmail.com>
2026-05-07 20:37:00 +0200

    erm also need here

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
     IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
     IPAddr.new("::/0"),             # All IPv6 addresses.
     "localhost",                    # The localhost reserved domain.
+    "kse.kirze.de",                 # Production domain.
     ENV["RAILS_DEVELOPMENT_HOSTS"]  # Additional comma-separated hosts for development.
   ]
 end
