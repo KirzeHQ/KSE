0b5edf2b9e7208e1022bf2705790e3744dc16bb2
SeradedStripes <seradedstripes@gmail.com>
2026-05-07 20:28:34 +0200

    add config.hosts idkl how it ran before

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config.hosts = [
+  IPAddr.new("0.0.0.0/0"),
+  IPAddr.new("::/0"),
+  "kse.kirze.de"
+]
